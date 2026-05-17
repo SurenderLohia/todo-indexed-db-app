a8b0b5a01ab8c6881cba26b4e0de01d19059b463
SurenderLohia <surender.lohia.e@gmail.com>
2026-05-17 20:30:31 +0530

    Store todos in Indexed db

--- components/TodoList/index.tsx
+++ components/TodoList/index.tsx
@@ -2,34 +2,59 @@
 
 import React, { useState, useEffect }  from "react"
 import { v4 as uuidv4 } from 'uuid';
+import { useLiveQuery } from 'dexie-react-hooks';
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 import { TodoItem, TodoItemData } from "@/components/TodoItem";
+import { db, TodoItemTypes } from "@/db";
+
+interface AddTodoToDB {
+  text: string,
+  isCompleted: boolean,
+}
 
 function TodoList() {
-  const [todos, setTodos] = useState<TodoItemData[]>([]);
+  //const [todos, setTodos] = useState<TodoItemData[]>([]);
   const [newTodo, setNewTodo] = useState("");
   const [selectedFilter, setSelectedFilter] = useState<"all" | "pending" | "completed">("all");
 
+  const todos = useLiveQuery(() => db.todos.toArray(), []) || [];
+
   const [isMounted, setIsMounted] = useState(false);
+  
   useEffect(() => {
     setIsMounted(true);
   }, []);
+
+  const addTodoToDB = async (todo: AddTodoToDB) => {
+    try {
+      await db.todos.add({...todo})
+    } catch (error) {
+      console.error("Error adding todo item to DB:", error);
+    }
+  }
 
   const handleAddTodo = (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (newTodo.trim() === "") return;
 
-    const newTodoItem: TodoItemData = {
-      id: uuidv4(),
+    // const newTodoItem: TodoItemData = {
+    //   id: uuidv4(),
+    //   text: newTodo,
+    //   isCompleted: false,
+    //   mode: 'view',
+    // };
+
+    const todoItem = {
       text: newTodo,
       isCompleted: false,
-      mode: 'view',
-    };
+    }
 
-    setTodos([...todos, newTodoItem]);
+    addTodoToDB(todoItem);
+
+    //setTodos([...todos, newTodoItem]);
     setNewTodo("");
   }
 
@@ -38,61 +63,61 @@ function TodoList() {
   }
 
   const toggleTodoItemComplete = (id: string) => {
-    setTodos(todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, isCompleted: !todo.isCompleted };
-      }
-      return todo;
-    }));
+    // setTodos(todos.map((todo) => {
+    //   if (todo.id === id) {
+    //     return { ...todo, isCompleted: !todo.isCompleted };
+    //   }
+    //   return todo;
+    // }));
   }
 
   const openTodoItemEditMode = (id: string) => {
-    setTodos(todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, mode: 'edit' };
-      }
-      return {
-        ...todo,
-        mode: 'view',
-      };
-    }));
+    // // setTodos(todos.map((todo) => {
+    //   if (todo.id === id) {
+    //     return { ...todo, mode: 'edit' };
+    //   }
+    //   return {
+    //     ...todo,
+    //     mode: 'view',
+    //   };
+    // }));
   }
 
   const closeTodoItemEditMode = (id: string) => {
-    setTodos(todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, mode: 'view' };
-      }
-      return todo;
-    }));
+    // setTodos(todos.map((todo) => {
+    //   if (todo.id === id) {
+    //     return { ...todo, mode: 'view' };
+    //   }
+    //   return todo;
+    // }));
   }
 
   const updateTodoItemText = (id: string, newText: string) => {
-    setTodos(todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, text: newText, mode: 'view' };
-      }
-      return todo;
-    }));
+    // setTodos(todos.map((todo) => {
+    //   if (todo.id === id) {
+    //     return { ...todo, text: newText, mode: 'view' };
+    //   }
+    //   return todo;
+    // }));
   }
 
   const deleteTodoItem = (id: string) => {
-    const updatedTodos = todos.filter(todo => todo.id !== id);
-    setTodos(updatedTodos);
+    // const updatedTodos = todos.filter(todo => todo.id !== id);
+    // setTodos(updatedTodos);
   }
 
-  const allCount = todos.length;
-  const pendingCount = todos.filter(todo => !todo.isCompleted).length;
-  const completedCount = todos.filter(todo => todo.isCompleted).length;
+  // const allCount = todos.length;
+  // const pendingCount = todos.filter(todo => !todo.isCompleted).length;
+  // const completedCount = todos.filter(todo => todo.isCompleted).length;
 
-  const filteredTodos = todos.filter(todo => {
-    if (selectedFilter === "pending") {
-      return !todo.isCompleted;
-    } else if (selectedFilter === "completed") {
-      return todo.isCompleted;
-    }
-    return true;
-  });
+  // const filteredTodos = todos.filter(todo => {
+  //   if (selectedFilter === "pending") {
+  //     return !todo.isCompleted;
+  //   } else if (selectedFilter === "completed") {
+  //     return todo.isCompleted;
+  //   }
+  //   return true;
+  // });
 
   if (!isMounted) return null;
   
@@ -102,7 +127,7 @@ function TodoList() {
         <Input type="text" id="add-task" placeholder="Add todo item" value={newTodo} onChange={onAddTodoChange} />
         <Button type="submit" variant="secondary">Add</Button>
       </form>
-      <div className="flex gap-3">
+      {/* <div className="flex gap-3">
         <Button size="sm" variant={selectedFilter === "all" ? "default" : "outline"} onClick={() => setSelectedFilter("all")}>
           All({allCount})
         </Button>
@@ -112,16 +137,16 @@ function TodoList() {
         <Button size="sm" variant={selectedFilter === "completed" ? "default" : "outline"} onClick={() => setSelectedFilter("completed")}>
           Completed({completedCount})
         </Button>
-      </div>
+      </div> */}
       <div className="flex flex-col gap-1">
-        {filteredTodos.map((todo) => (
+        {todos.map((todo) => (
           <TodoItem 
             key={todo.id} 
             id={todo.id} 
             text={todo.text}
             isCompleted={todo.isCompleted}
             toggleTodoItemComplete={toggleTodoItemComplete}
-            mode={todo.mode}
+            mode="view"
             openTodoItemEditMode={openTodoItemEditMode}
             updateTodoItemText={updateTodoItemText}
             closeTodoItemEditMode={closeTodoItemEditMode}
---
+++ db.tsx
@@ -0,0 +1,19 @@
+import { Dexie, type EntityTable } from "dexie";
+
+interface TodoItemTypes  {
+  id: string,
+  text: string,
+  isCompleted: boolean,
+}
+
+const db = new Dexie("TodoDatabase") as Dexie & {
+  todos: EntityTable<TodoItemTypes, "id">
+}
+
+// Schema declaration:
+db.version(1).stores({
+  todos: "++id, text, isCompleted"
+});
+
+export type { TodoItemTypes }
+export { db }
